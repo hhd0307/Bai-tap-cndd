5061e1c4ba79297834d159e35b141b75dd0fd820
Duc Huynh <hhd20396@gmail.com>
2018-02-04 20:34:23 +0700

    BT Giao dien 1

---
+++ GiaoDien01/app/src/main/java/com/hoangduc/giaodien01/MainActivity.java
@@ -0,0 +1,16 @@
+package com.hoangduc.giaodien01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_01);
+//        setContentView(R.layout.activity_02);
+//        setContentView(R.layout.activity_03);
+        setContentView(R.layout.activity_04);
+    }
+}
